217af720302f44e9da32232aead997ed7696553d
Máximo de Jesús <maximojev@hotmail.com>
2024-11-11 21:19:30 -0600

    feat: Adding CRUD view and sorting section for reindeers

---
+++ src/components/reindeer/ReindeerComboBox.jsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import { Check, SquarePlus } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export default function ReindeerComboBox(props) {
+  const { reindeers, value: defaultValue } = props;
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(
+    defaultValue > 0 && defaultValue < 7 ? defaultValue : ""
+  );
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className=" justify-between "
+        >
+          {value ? (
+            reindeers.find((reindeer) => reindeer.id === value)?.name
+          ) : (
+            <SquarePlus className="text-amber-900" />
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0">
+        <Command>
+          <CommandInput placeholder="Search reindeer..." className="h-9" />
+          <CommandList>
+            <CommandEmpty>No reindeer found.</CommandEmpty>
+            <CommandGroup>
+              {reindeers.map((reindeer) => (
+                <CommandItem
+                  key={reindeer.id}
+                  value={reindeer.name}
+                  onSelect={() => {
+                    setValue(reindeer.id === value ? "" : reindeer.id);
+                    setOpen(false);
+                  }}
+                >
+                  {reindeer.name}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === reindeer.id ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
---
+++ src/services/reindeer/reindeerapi.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+
+// const URL = import.meta.env.VITE_API_URL;
+const MOCKURL = import.meta.env.VITE_MOCK_API_URL;
+
+// fetch all reindeers
+export const useReindeers = () => {
+  return useQuery({
+    queryKey: ["reindeers"],
+    queryFn: async () => {
+      const { data } = await axios.get(`${MOCKURL}/allReindeers`);
+      return data;
+    },
+  });
+};
+
+// add new reindeer
+export const useAddReindeer = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (newReindeer) =>
+      axios.post(`${MOCKURL}/allReindeers`, newReindeer),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["reindeers"] });
+    },
+  });
+};
+
+// update reindeer
+export const useUpdateReindeers = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (updatedreindeer) =>
+      axios.put(
+        `${MOCKURL}/allReindeers/${updatedreindeer.id}`,
+        updatedreindeer
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["reindeers"] });
+    },
+  });
+};
