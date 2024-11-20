09b807d2ed740572d87a55f3ba9833603340e784
Ing. Roberto J. Vargas <rvargas@rv-solutions.net>
2024-11-20 13:28:55 -0500

    Initial Commit - Bugfix of the first Review

--- src/components/reindeer/WeatherCard.jsx
+++ src/components/reindeer/WeatherCard.jsx
@@ -1,4 +1,4 @@
-/* import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { Card } from "@/components/ui/card";
 import { Snowflake } from "lucide-react";
 import { fetchWeather } from "@/services/weather/weather";
@@ -45,4 +45,3 @@ export const WeatherCard = () => {
     </Card>
   );
 };
- */
